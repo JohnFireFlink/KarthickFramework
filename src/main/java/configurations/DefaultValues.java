b1087b40b2e9996635469c3a3a3f8116324793f6
JohnFireFlink <160256833+JohnFireFlink@users.noreply.github.com>
2025-10-12 18:27:33 +0530

    Updated send mail feature and removed hardcoded API key

--- src/main/java/configurations/DefaultValues.java
+++ src/main/java/configurations/DefaultValues.java
@@ -8,6 +8,4 @@ public interface DefaultValues {
 	String FROMEMAIL="johnmathewtrainer@gmail.com";
 	String BROWSER="chrome";
 	String URL="https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
-	String APIKEY="U0cuQV9hbnl5NGVTcWl6SkJUcV9yRHNQQS5RbDBFd3F3TWwwal9KbUg2azNzVWtNT3cwQVFqMmx2ZXdaSXQ3MHRJbHEw";
-	
 }
